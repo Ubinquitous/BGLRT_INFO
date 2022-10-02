736546ad9ccc1b3901ce13056fa4263b60ccd5cd
Ubinquitous <ubinquitous1@gmail.com>
2022-10-02 19:56:28 +0900

    ADD : 구성 요소 전체 완성

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,88 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Header />
+    <div :key="i" v-for="i in 20" id="trail_box_wrap">
+      <div class="trail_wrap">
+        <h1 class="station">{{ name.data[i] }}역</h1>
+        <h1 class="stations">{{ station.data[i] }}</h1>
+        <h1 class="address">도로명주소 : {{ address.data[i]}}</h1>
+        <h1 class="location">지번주소 : {{ location.data[i]}}</h1>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import { Options, Vue } from 'vue-class-component'
+import Header from '@/components/Header.vue' // @ is an alias to /src
+
+@Options({
+    components: {
+      Header
+    }
+  })
+export default class extends Vue {
+
+  data(){
+    return {
+      name: { data:[] },
+      address: { data:[] },
+      location: { data:[] },
+      station: { data:[] },
+    }
+  }
+  mounted(){
+    const url1 = 'https://api.odcloud.kr/api/15041096/v1/uddi:e83cfa23-23e6-4d02-a061-cb427648c51e?page=1&perPage=22&serviceKey=suyekkuR7SRO2720g7tgje0Nr1frEuxvzTGqC29kOGrjWzm8b3jdikE0%2Fz4HtHCm3YSdxOp%2FLnAtzPwwATb6lg%3D%3D';
+    try {
+        fetch(url1).then(response => response.json())
+        .then(data =>{
+          console.log(data.data[0])
+          for(let i=0; i<=20; i++){
+            this.name.data[i] = data.data[i].역명.slice(0, data.data[i].역명.indexOf('(') === -1 ? 10 : data.data[i].역명.indexOf('('));
+            this.address.data[i] = data.data[i].도로명주소
+            this.location.data[i] = data.data[i].지번주소
+            this.station.data[i] = data.data[i].선명
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+  }
+}
+</script>
+<style scoped>
+  #trail_box_wrap {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+  .trail_wrap {
+    width:50vw;
+    height:90px;
+    border-top:2px solid #ccc;
+    margin: 20px 0px;
+  }
+
+  .station {
+    font-size: 30px;
+    margin:10px;
+  }
+
+  .address {
+    font-size: 16px;
+  }
+
+  .location {
+    font-size: 16px;
+  }
+
+  .stations {
+    margin-top: -10px;
+    font-size: 20px;
+  }
+
+  .hidden {
+    display: none;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="home">
-    <Header msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Header />
     <div class="background_wrap">
       <h1 class="title">Please search for a station.</h1>
       <div class="background_input_wrap">
         <input type="text" class="search_input" v-model="values"/>
-        <router-link to="/Detail" class="search_btn">
           <button class="search_btn_detail" @click="Details">Search</button>
-
-        </router-link>
       </div>
     </div>
   </div>
@@ -27,22 +24,31 @@
 export default class extends Vue {
 
   Details(){
-    alert(this.values);
-  }
-
-  fetchTodo() {
+    let count = 0;
     const url1 = 'https://api.odcloud.kr/api/15041096/v1/uddi:e83cfa23-23e6-4d02-a061-cb427648c51e?page=1&perPage=22&serviceKey=suyekkuR7SRO2720g7tgje0Nr1frEuxvzTGqC29kOGrjWzm8b3jdikE0%2Fz4HtHCm3YSdxOp%2FLnAtzPwwATb6lg%3D%3D';
     try {
         fetch(url1).then(response => response.json())
         .then(data =>{
-          console.log(data.data[0]);
-          // for(let i=0; i<21; i++){
+          for(let i=0; i<=20; i++){
+            if(this.values === data.data[i].역명.slice(0, data.data[i].역명.indexOf('(') === -1 ? 10 : data.data[i].역명.indexOf('('))){
+              alert(`
+              ${data.data[i].역명}역
+              도로명주소 : ${data.data[i].도로명주소} 
+              지번주소 : ${data.data[i].지번주소}
+              철도운영기관명 : ${data.data[i].역명}
             
-          // }
+              모든 경전철의 세부 정보를 원하신다면 타이틀을 클릭해주세요.`)
+            } else {
+              count++;
+            }
+          }
+          if(count===21){
+            alert('역을 찾을 수 없습니다. 정확하게 입력해주세요.')
+          }
         });
       } catch (error) {
-      console.log(error);
-    }
+        console.log(error);
+      }
   }
   data(){
     return{
